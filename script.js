32796326eda5e687a8fe408999cc9cd5089c814b
dubeyjaya25 <jaya24dubey@gmail.com>
2025-07-27 11:12:32 +0530

    Add code for timmer in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// for offer timer
+
+let timeLeft = 360; // 1 hour in seconds
+const timerDisplay = document.getElementById('timer');
+
+function formatTime(seconds) {
+  const hrs = String(Math.floor(seconds / 3600)).padStart(2, '0');
+  const mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+  const secs = String(seconds % 60).padStart(2, '0');
+  return `${hrs}:${mins}:${secs}`;
+}
